4eef2966a8bc05544e96ede02dd6b4936158703a
Leonardo Dias <leonardodias@ucl.br>
2021-10-06 18:20:55 -0300

    Create classroom class. Create relationships between classroom and student classes

--- classes/student.rb
+++ classes/student.rb
@@ -1,11 +1,18 @@
 require './person'
 
 class Student < Person
-  def initialize(classroom)
+  def initialize(age, name, classroom: nil)
     super(age, name, parent_permission: true)
     @classroom = classroom
   end
 
+  attr_reader :classroom
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+    
   def play_hooky
     "¯\(ツ)/¯"
   end
